cdc5e95236e84b8f1d109e6187f85b15841bf982
tilakgupta2005 <tilakgupta2005@gmail.com>
2026-05-16 21:55:36 +0530

    Implemeted user creation and login to use hashed passwords for improved security

--- services/auth_service.py
+++ services/auth_service.py
@@ -1,5 +1,7 @@
 from schema.users import *
 from core.database import get_connection
+import bcrypt
+
 
 def check_user_exists(email: str) -> bool:
     conn = get_connection()
@@ -16,24 +18,31 @@ def create_user(user_data: CreateUser):
         raise Exception("User with this email already exists")
     conn = get_connection()
     cursor = conn.cursor()
+    hashed_password = bcrypt.hashpw(user_data.password.encode('utf-8'), bcrypt.gensalt())
     query = "INSERT INTO users (Name, Email, password) VALUES (%s, %s, %s)"
-    cursor.execute(query, (user_data.Name, user_data.Email, user_data.password))
+    cursor.execute(query, (user_data.Name, user_data.Email, hashed_password))
     conn.commit()
     cursor.close()
     conn.close()
     print(f"Creating user: {user_data.Name}, Email: {user_data.Email}")
 
-
+  
 def login_user(user_data: LoginUser):
     if not check_user_exists(user_data.Email):
         raise Exception("User with this email does not exist")
     conn = get_connection()
     cursor = conn.cursor()
-    query = "SELECT * FROM users WHERE Email = %s AND password = %s"
-    cursor.execute(query, (user_data.Email, user_data.password))
+    query = "SELECT * FROM users WHERE Email = %s"
+    cursor.execute(query, (user_data.Email,))
     user = cursor.fetchone()
     cursor.close()
     conn.close()
     if not user:
         raise Exception("Invalid email or password")
+        
+    # Compare input password with stored hash
+    if not bcrypt.checkpw(user_data.password.encode('utf-8'), user[3].encode('utf-8')):
+        raise Exception("Invalid email or password")
+        
     print(f"Logging in user with Email: {user_data.Email}")
+    
